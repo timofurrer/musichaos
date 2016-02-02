3d50a3fcd35da35f8aade602d0e280a4421d3445
Timo Furrer <tuxtimo@gmail.com>
2016-02-01 16:17:18 -0800

    Fix cli help messages

--- musichaos/__main__.py
+++ musichaos/__main__.py
@@ -14,8 +14,8 @@ from .errors import MusichaosError
 @click.command()
 @click.argument("rootdir", nargs=1, type=click.Path(exists=True))
 @click.argument("targetdir", default=None, type=click.Path(exists=True))
-@click.option("-l", "--link", is_flag=True)
-@click.option("--dry-run", is_flag=True)
+@click.option("-l", "--link", is_flag=True, help="Link audio files isntead of move.")
+@click.option("--dry-run", is_flag=True, help="Run a dry run.")
 def main(rootdir, targetdir, link, dry_run):
     """
         Tidy up your music chaos.
