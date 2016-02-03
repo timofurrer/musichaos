d10cf1d5ba42d567954ef95c762bd6eed2eb3c27
Timo Furrer <tuxtimo@gmail.com>
2016-02-02 19:42:13 -0800

    release: 0.1.0

--- musichaos/__init__.py
+++ musichaos/__init__.py
@@ -4,7 +4,7 @@
     Package to tidy up your music chaos.
 """
 
-__VERSION__ = "0.0.1"
+__VERSION__ = "0.1.0"
 __LICENSE__ = "MIT"
 __AUTHOR__ = "Timo Furrer"
 __AUTHOR_EMAIL__ = "tuxtimo@gmail.com"
